7c702158fa5210eb0bfb48588711fa94f2c6dbe0
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-07-18 20:34:38 +0530

    Complete MovieCard component and integrate it with App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import MovieCart from './components/MovieCard.jsx'
 
 function App() {
   return (
---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+function MovieCart({ movie }) {
+    function onFavoriteClick() {
+        alert("Clicked on favorite");
+    }
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.Title} />
+            </div>
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onFavoriteClick}>🤍</button>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.Title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCart;
